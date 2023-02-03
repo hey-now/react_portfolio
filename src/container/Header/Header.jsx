d06d891ba663859a86acd3ef78b9668c1deb7ae1
hey-now <rubinand22@gmail.com>
2023-02-03 14:10:02 -0800

    adding HOC for Nav dots and Social media

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 
+import { AppWrap } from '../../wrapper';
+
 import './Header.scss';
 
 const Header = () => {
@@ -16,7 +18,7 @@ const Header = () => {
     }
   }
   return (
-    <div id='home' className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 1 }}
@@ -71,4 +73,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, 'home');
