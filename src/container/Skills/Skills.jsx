7a27c3f8070f638d4a5c33ee638b2264c8f0159a
hey-now <rubinand22@gmail.com>
2023-02-04 19:25:37 -0800

    adding sorting to skills

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
-    const skillsQuery = '*[_type == "skills"]';
+    const skillsQuery = '*[_type == "skills"] | order(name asc)';
 
     client.fetch(query)
       .then((data) => {
